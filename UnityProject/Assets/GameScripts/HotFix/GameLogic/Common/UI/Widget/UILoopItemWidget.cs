ea27bfe5205798b0fa65d5b70b1b182c4c6e1d75
zhoupeng <zhoupengzy@163.com>
2026-06-23 17:46:34 +0800

    添加superscrollview

---
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/Common/UI/Widget/UILoopItemWidget.cs
@@ -0,0 +1,12 @@
+﻿namespace GameLogic
+{
+    public class UILoopItemWidget : SelectItemBase
+    {
+        public LoopListViewItem LoopItem { set; get; }
+        public int Index { private set; get; }
+        public virtual void UpdateItem(int index)
+        {
+            Index = index;
+        }
+    }
+}
